277ead0b412ddf78fcd6cf61ee34abbcea8dd478
Kevin <kvnnedd@gmail.com>
2026-04-25 21:14:20 -0400

    chore: initialized project with tailwind v4, custom fonts and relevant assets

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+      <main>
+        <h1 className="font-zentry text-5xl text-violet-300 font-bold">Welcome to awwards</h1>
+      </main>
+  )
+}
+export default App
