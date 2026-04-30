95f781eec6ced4a5dcc82cf6839ef7ef5ac6674e
Kevin <kvnnedd@gmail.com>
2026-04-29 23:28:52 -0400

    feat(Story) Create story component and add to app jsx - Add animated title component to story section - style and structure story section - create animation for the image in the story section

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
 
 const App = () => {
   return (
       <main className="relative min-h-screen w-screen overflow-x-hidden">
         <Navbar/>
         <Hero/>
-
         <About/>
         <Features/>
+        <Story/>
       </main>
   )
 }
