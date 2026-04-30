31830ecaa134cc05a277551e1d0605226e951cd6
Kevin <kvnnedd@gmail.com>
2026-04-30 00:11:46 -0400

    - create rounded corners component and import into story component - import Button component into Story Component - add image to story section - create Contact and Footer component and import into App.jsx

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,6 +1,8 @@
 import AnimatedTitle from "./AnimatedTitle.jsx";
 import {useRef} from "react";
 import gsap from 'gsap';
+import RoundedCorners from "./RoundedCorners.jsx";
+import Button from "./Button.jsx";
 
 const Story = () => {
   const frameRef = useRef(null);
@@ -42,26 +44,41 @@ const Story = () => {
 
   }
   return (
-      <section id={"story"} className={"min-h-screen w-screen bg-black text-blue-50"}>
-        <div className={"flex size-full flex-col items-center py-10 pb-24"}>
-          <p className={"font-general text-sm uppercase md:text-[10px]"}>
+      <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+        <div div className="flex size-full flex-col items-center py-10 pb-24">
+          <p className="font-general text-sm uppercase md:text-[10px]">
             The multiversal ip world
           </p>
           <div className="relative size-full">
             <AnimatedTitle
-            title="The St<b>o</b>ry of <br /> a hidden real<b>m</b>"
+                title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+                containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
             sectionId={"#story"}
-            containerClass={"mt-5 pointer-events-none mix-blend-difference relative z-10"}
             />
-            <div className={"story-img-container"}>
+            <div className="story-img-container">
               <div className="story-img-mask">
                 <div className="story-img-content">
-                  <img ref={frameRef} onMouseLeave={handleMouseLeave} onMouseUp={handleMouseLeave} onMouseEnter={handleMouseLeave} onMouseMove={handleMouseMove} src={"/img/entrance.webp"} alt={"entrance"} className={"object-contain"}/>
+                  <img
+                      ref={frameRef}
+                      onMouseLeave={handleMouseLeave}
+                      onMouseUp={handleMouseLeave}
+                      onMouseEnter={handleMouseLeave}
+                      onMouseMove={handleMouseMove}
+                      src={"/img/entrance.webp"}
+                      alt={"entrance"}
+                      className={"object-contain"}/>
                 </div>
               </div>
-
+              <RoundedCorners />
             </div>
-
+          </div>
+          <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+            <div className="flex h-full w-fit flex-col items-center md:items-start">
+              <p className={"mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start"}>
+                Where realms converge, lies Zentry and the boundless pillar. Discover its secrets and shape your gate amidst infinite opportunities.
+              </p>
+              <Button id="realm-button" title="discover prologue" containerClass={"mt-5"}/>
+            </div>
           </div>
         </div>
       </section>
