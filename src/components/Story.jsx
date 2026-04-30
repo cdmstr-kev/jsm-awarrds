187ce23e0507b56a52527d9f6841e414987e26d2
Kevin <kvnnedd@gmail.com>
2026-04-30 15:33:02 -0400

    feat:Contact finish contact component and fex minor bug in story component. - implement contact us section with all images - resolved a minor bug with a duplicated div in story component

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -45,7 +45,7 @@ const Story = () => {
   }
   return (
       <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
-        <div div className="flex size-full flex-col items-center py-10 pb-24">
+        <div className="flex size-full flex-col items-center py-10 pb-24">
           <p className="font-general text-sm uppercase md:text-[10px]">
             The multiversal ip world
           </p>
